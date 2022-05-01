157fe708f40dde5e18e540f3211867acc2a7df33
ramza81 <bandiera81@gmail.com>
2022-05-01 17:10:52 +0900

    라우터 기본

---
+++ src/app.js
@@ -0,0 +1,20 @@
+// // 1
+// var a = 10;
+
+// // 2
+// var b = 10
+
+// import AppHeader from './components/AppHeader'
+// import Vue from 'vue';
+// import { component } from 'vue/types/umd';
+
+// if (a === 10) {
+//     console.log('10이다');
+// }
+
+// if (a === 1) console.log('10이다') b() c()
+
+// // 트레이링 콤마 - trailing comma
+// component: {
+//     '컴포넌트 이름': 컴포넌트 내용,
+// }
---
+++ src/routes/index.js
@@ -0,0 +1,42 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import NewsView from '../views/NewsView.vue';
+import AskView from '../views/AskView.vue';
+import JobsView from '../views/JobsView.vue';
+import ItemView from '../views/ItemView.vue';
+import UserView from '../views/UserView.vue';
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+        path: '/',
+        redirect: '/news',
+    },      
+    {
+        // path : url address
+        // component : url 주소로 갔을 때 표시될 컴포넌트
+        path: '/news',
+        component: NewsView,
+    },
+    {
+        path: '/ask',
+        component: AskView,
+    },
+    {
+        path: '/jobs',
+        component: JobsView,
+    },    
+    {
+      path: '/item',
+      component: ItemView,
+    },   
+    {
+      path: '/user',
+      component: UserView,
+    },         
+  ]
+});
+
