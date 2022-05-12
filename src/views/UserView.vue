03aa0f06b73c897bf8c463dc105e6afa9fa7ed7a
ramza81 <bandiera81@gmail.com>
2022-05-12 21:43:41 +0900

    사용자 컴포넌트 데이터 흐름 처리

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,19 +1,24 @@
 <template>
   <div>
-      <p>name : {{ userInfo.id }}</p>
+    <UserProfile :info="userInfo"></UserProfile>
+      <!-- <p>name : {{ userInfo.id }}</p>
       <p>karma : {{ userInfo.karma }}</p>
       <p>created : {{ userInfo.created }}</p>
       <p>name : {{ fetchedUser.id }}</p>
       <p>karma : {{ fetchedUser.karma }}</p>
-      <p>created : {{ fetchedUser.created }}</p>      
+      <p>created : {{ fetchedUser.created }}</p>       -->
   </div>
 </template>
 
 <script>
 
 import { mapGetters } from 'vuex';
+import UserProfile from '../components/UserProfile.vue';
 
 export default {
+  components: {
+    UserProfile, 
+  },
   computed: {
     ...mapGetters([
       'fetchedUser'
