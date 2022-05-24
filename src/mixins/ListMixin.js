faa222fa8a82bdfc352c33a95fe2ae2571058daa
ramza81 <bandiera81@gmail.com>
2022-05-24 16:54:41 +0900

    init

--- src/mixins/ListMixin.js
+++ src/mixins/ListMixin.js
@@ -1,10 +1,7 @@
-import ListView from "./ListView.vue";
 import bus from '../utils/bus.js';
 
-export default function createListView(name) {
-    return  {
-        // 재사용할 인스턴스(컴포넌트) 옵션들이 들어갈 자리
-        name,
+export default {
+    // 재사용할 컴포넌트 옵션 & 로직
         created() {
             bus.$emit('start:spinner');
             this.$store.dispatch('FETCH_LIST', this.$route.name)
@@ -14,9 +11,6 @@ export default function createListView(name) {
             .catch((error) => {
             console.log(error);
             });
-        },
-        render(createLelement) {
-            return createLelement(ListView);
-        }
-    }
+        },    
+
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import NewsView from '../views/NewsView.vue';
+// import AskView from '../views/AskView.vue';
+// import JobsView from '../views/JobsView.vue';
 import ItemView from '../views/ItemView.vue';
 import UserView from '../views/UserView.vue';
 import createListView from "../views/CreateListView.js";
@@ -18,17 +21,20 @@ export const router = new VueRouter({
         // component : url 주소로 갔을 때 표시될 컴포넌트
         path: '/news',
         name: 'news',
-        component: createListView('NewView'),
+        // component: createListView('NewView'),
+        component: NewsView,
     },
     {
         path: '/ask',
         name: 'ask',
         component: createListView('AskView'),
+        // component: AskView,
     },
     {
         path: '/jobs',
         name: 'jobs',
         component: createListView('JobsView'),
+        // component: JobsView,
     },    
     {
       path: '/item/:id',
